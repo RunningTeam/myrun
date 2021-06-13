0e189d1a5186088961e0fe47d2cbe5d38c142863
woogie96 <fjdi789@gachon.ac.kr>
2021-06-14 04:19:00 +0900

    edit login

--- app/src/main/java/com/example/myrun/model/User.java
+++ app/src/main/java/com/example/myrun/model/User.java
@@ -7,6 +7,9 @@ public class User {
     private String userNickName; // 유저 닉네임
     private Timestamp registerTime; // 가입한 시간
 
+    // 파라미터 없는 생성자
+    public User() {}
+    // 파라미터 있는 생성자
     public User(String userEmail, String userNickName, Timestamp registerTime){
         this.userEmail = userEmail;
         this.userNickName = userNickName;
