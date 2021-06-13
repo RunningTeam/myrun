509cab611da63259e1641e070643695197b2212b
woogie96 <fjdi789@gachon.ac.kr>
2021-06-14 01:46:03 +0900

    edit game

--- app/src/main/java/com/example/myrun/GamingActivity.java
+++ app/src/main/java/com/example/myrun/GamingActivity.java
@@ -23,8 +23,10 @@ import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.overlay.PathOverlay;
 import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.util.MarkerIcons;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -152,6 +154,11 @@ public class GamingActivity extends AppCompatActivity implements OnMapReadyCallb
         if (locationList.size() > 2) {
             path.setColor(Color.GREEN);
             path.setMap(mNaverMap);
+            Marker marker = new Marker();
+            marker.setIcon(MarkerIcons.BLACK);
+            marker.setIconTintColor(Color.RED);
+            marker.setPosition(locationList.get(locationList.size()-2));
+            marker.setMap(mNaverMap);
         }
         mNaverMap.setLocationSource(mLocationSource);
         mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
